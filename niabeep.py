249de0d5522f5f364807c030eef8d46aaeb69e36
KenjiDoom <email@example.com>
2021-08-17 20:21:51 -0700

    displays text and email strings

--- niabeep.py
+++ niabeep.py
@@ -6,22 +6,29 @@ class app1:
     def __init__(self, master):
         self.master = master
         self.frame = tk.Frame(self.master)
-        #self.frame.pack(anchor='center')
         self.master.title('Authentication')
 
-        self.frame1 = tk.Frame(self.master, width=250, height=250, relief='raise', bd=8)
+        # The big white frame
+        self.frame1 = tk.Frame(self.master, width=250, height=270, relief='raise', bd=8)
         self.frame1.place(relx=0.5, rely=0.4, anchor='center')
 
-        # Login Login Text
-        self.login_message = tk.Label(self.frame1, width=20, text='Login into your account', relief='raise', font=("Helvetica", 14))
-        self.login_message.place(relx=0.5, rely=0.10, anchor="center")
+        # Login Message Text
+        self.login_message = tk.Label(self.frame1, width=20, text='Login into your account', relief='raise', font=("Times", 14))
+        self.login_message.place(relx=0.5, rely=0.05, anchor="center")
+
+        self.email_message = tk.Label(self.frame1, width=10, text='Email: ', relief='flat', font=("Times", 11))
+        self.email_message.place(relx=0.15, rely=0.28, anchor="center")
 
         # Email Entry
-        self.email = tk.Entry(self.frame1, text='Email', bd=1, width=20)
+        self.email = tk.Entry(self.frame1, text='Email', bd=1, width=25)
         self.email.place(relx=0.5, rely=0.35, anchor="center")
 
+        # Password Message Text
+        self.password_message = tk.Label(self.frame1, width=10, text='Password: ', relief='flat', font=("Times", 11))
+        self.password_message.place(relx=0.20, rely=0.52, anchor="center")
+
         # Password Entry
-        self.password = tk.Entry(self.frame1, text='Password', bd=1, width=20)
+        self.password = tk.Entry(self.frame1, text='Password', bd=1, width=25)
         self.password.place(relx=0.5, rely=0.60, anchor="center")
 
         # Login Button
